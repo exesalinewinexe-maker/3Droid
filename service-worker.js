423b09ada830262323b76fba2f56bbc3487e96ec
exesalinewinexe-maker <exesalinewin.exe@gmail.com>
2026-05-24 20:50:57 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,5 +7,13 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+  // ensure requests inside /3Droid/ work correctly
+  const url = new URL(event.request.url);
+
+  if (url.pathname.startsWith('/3Droid/')) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
   event.respondWith(fetch(event.request));
 });
