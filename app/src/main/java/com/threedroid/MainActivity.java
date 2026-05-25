3969967e39b243bba9e7063e389d02efa2f55cfe
exesalinewinexe-maker <exesalinewin.exe@gmail.com>
2026-05-24 20:09:36 -0700

    Update MainActivity.java

--- app/src/main/java/com/threedroid/MainActivity.java
+++ app/src/main/java/com/threedroid/MainActivity.java
@@ -1,13 +1,15 @@
-package com.threedroid;
+package com.threedroid.emulator;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EmulatorCore core = new EmulatorCore();
+        core.start();
     }
 }
